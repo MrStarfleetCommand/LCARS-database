d8082a9ab14e92a841dcb3718523ddd375ef9392
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2025-07-03 21:32:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,362 @@
+document.getElementById('preload').innerHTML = `
+<div class="waveform">
+  <div class="element-one"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="line"></div>
+  <div class="element-two"></div>
+</div>
+<div class="button sidebar-1 left-facing red" onclick="beep0()"><span class="margin">03-975683</span></div>
+<div class="button sidebar-2 left-facing light-gray" onclick="beep0()"><span class="margin">04-765466</span></div>
+<div class="button sidebar-3 left-facing dark-blue" onclick="beep0()"><span class="margin">05-224353</span></div>
+<div class="button sidebar-4 square border-button" onclick="beep2()"><span class="margin">03-975683</span></div>
+<div class="button sidebar-5 square border-button" onclick="beep2()"><span class="margin">04-765466</span></div>
+<div class="button sidebar-6 square border-button-cyan" onclick="beep1()"><span class="margin">05-224353</span></div>
+<div class="version-info">
+  <ul>
+    <li>Operating Version: v104.04.720</li>
+    <li>Last Updated: 2391-01-01T19:54:17Z (68024.71)</li>
+    <li>Latest Version: v118.77.022</li>
+    <li>Latest Release: 2405-07-20T22:17:40Z (82566.00)</li>
+    <li>Current Operational Load: 6 Kiloquads</li>
+  </ul>
+</div>
+<div class="button sidebar-7 left-facing red blink-slow" onclick="beep0()"><span class="margin">03-975683</span></div>
+<div class="button sidebar-8 left-facing light-blue blink-fast" onclick="beep0()"><span class="margin">04-765466</span></div>
+<div class="button sidebar-9 left-facing dark-gray" onclick="beep0()"><span class="margin">05-224353</span></div>
+<div class="button sidebar-10 right-facing light-gray" onclick="beep0()"><span class="margin">03-975683</span></div>
+<div class="button sidebar-11 right-facing red" onclick="beep0()"><span class="margin">04-765466</span></div>
+<div class="button sidebar-12 right-facing dark-blue" onclick="beep0()"><span class="margin">05-224353</span></div>
+<div class="panel2 one light-blue"><span class="margin">LCARS 40274</span></div>
+<div class="panel2 two light-gray"><span class="margin">02-654598</span></div>
+<div class="panel2 three dark-blue"><span class="margin">03-975683</span></div>
+<div class="panel2 four light-gray"><span class="margin">04-765466</span></div>
+<div class="panel2 five dark-gray"><span class="margin">05-224353</span></div>
+<div class="panel2 six light-gray"><span class="margin">06-576565</span></div>
+<div class="panel2 seven dark-blue"><span class="margin">07-395476</span></div>
+<div class="panel2 eight red"><span class="margin">08-849204</span></div>
+<div class="panel2 nine light-gray"><span class="margin">09-938495</span></div>
+<div class="panel2 ten dark-blue"><span class="margin">10-194829</span></div>
+<div class="panel one light-blue"><span class="margin">LCARS 40274</span></div>
+<div class="panel two dark-blue"><span class="margin">02-654598</span></div>
+<div class="panel three dark-blue"><span class="margin">03-975683</span></div>
+<div class="panel four light-gray"><span class="margin">04-765466</span></div>
+<div class="panel five light-gray"><span class="margin">05-224353</span></div>
+<div class="panel six light-blue"><span class="margin">06-576565</span></div>
+<div class="panel seven red"><span class="margin">07-395476</span></div>
+<div class="panel eight red"><span class="margin">08-849204</span></div>
+<div class="panel nine light-gray"><span class="margin">09-938495</span></div>
+<div class="panel ten dark-blue"><span class="margin">10-194829</span></div>
+<h1>LCARS Database</h1>
+<table class="cascade">
+  <tr>
+    <td>2385</td>
+    <td></td>
+    <td>8578232</td>
+    <td>9</td>
+    <td>5789</td>
+    <td>3882</td>
+    <td>5893</td>
+    <td>9885</td>
+    <td>3489</td>
+    <td>3465</td>
+    <td>0846</td>
+    <td>9798</td>
+    <td>9629</td>
+    <td>29</td>
+    <td>34</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>279</td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+  </tr>
+  <tr>
+    <td>2064</td>
+    <td></td>
+    <td>2064962</td>
+    <td>7</td>
+    <td>9776</td>
+    <td>626</td>
+    <td>1276</td>
+    <td>7612</td>
+    <td>126</td>
+    <td>97</td>
+    <td>6165</td>
+    <td>6626</td>
+    <td>876</td>
+    <td>74</td>
+    <td>2385</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>9</td>
+    <td>5789</td>
+    <td>3882</td>
+    <td>5893</td>
+    <td>3489</td>
+    <td>3465</td>
+    <td>0846</td>
+    <td>9798</td>
+    <td>9629</td>
+    <td>29</td>
+    <td>4588</td>
+  </tr>
+  <tr>
+    <td>34</td>
+    <td></td>
+    <td>279</td>
+    <td></td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>2064</td>
+    <td>9776</td>
+    <td>626</td>
+    <td>1276</td>
+    <td>7612</td>
+    <td>126</td>
+    <td>97</td>
+    <td>6165</td>
+    <td>6626</td>
+    <td>876</td>
+    <td>74</td>
+    <td>3489</td>
+  </tr>
+  <tr>
+    <td>4768</td>
+    <td></td>
+    <td>8967248</td>
+    <td>7</td>
+    <td>9798</td>
+    <td>8969</td>
+    <td>476</td>
+    <td>9047</td>
+    <td>8476</td>
+    <td>9749</td>
+    <td>0982</td>
+    <td>8969</td>
+    <td>0247</td>
+    <td>89</td>
+    <td>34</td>
+    <td>279</td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+  </tr>
+  <tr>
+    <td>685</td>
+    <td></td>
+    <td>3478</td>
+    <td>8</td>
+    <td>867</td>
+    <td>346</td>
+    <td>34</td>
+    <td>48</td>
+    <td>49</td>
+    <td>8</td>
+    <td>89</td>
+    <td>897</td>
+    <td>38</td>
+    <td>34</td>
+    <td>279</td>
+    <td></td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+  </tr>
+  <tr>
+    <td>757</td>
+    <td></td>
+    <td>898990</td>
+    <td>8</td>
+    <td>200</td>
+    <td>285</td>
+    <td>923</td>
+    <td>9</td>
+    <td>387</td>
+    <td>238</td>
+    <td>578</td>
+    <td>875</td>
+    <td>87</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>9</td>
+    <td>34</td>
+    <td></td>
+    <td>279</td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>05</td>
+  </tr>
+  <tr>
+    <td>484</td>
+    <td></td>
+    <td>947589</td>
+    <td>7</td>
+    <td>569</td>
+    <td>68</td>
+    <td>678</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>893</td>
+    <td>56</td>
+    <td>584</td>
+    <td>678</td>
+    <td>476</td>
+    <td>458</td>
+    <td>4</td>
+    <td>34</td>
+    <td>279</td>
+    <td>89</td>
+    <td>6589</td>
+    <td>6547</td>
+    <td>6587</td>
+    <td>3465</td>
+    <td>867</td>
+    <td>2347</td>
+    <td>5762</td>
+    <td>4588</td>
+    <td>05</td>
+  </tr>
+</table>
+<a class="button header-one dark-blue" href="index.html" onclick="beep0()"><span class="margin">Home</span></a>
+<a class="button header-two light-blue" href="timeline/index.html" onclick="beep0()"><span class="margin">Timeline</span></a>
+<a class="button header-three red" href="trek-analyzed/index.html" onclick="beep0()"><span class="margin">Trek Analyzed</span></a>
+<a class="button header-four light-gray" href="trek-lore/index.html" onclick="beep0()"><span class="margin">Trek Lore</span></a>
+<div class="bar number-one dark-blue"></div>
+<div class="bar number-two dark-blue"></div>
+<div class="bar-break light-gray"></div>
+<div class="bar-continued number-one light-gray"></div>
+<div class="bar-continued number-two light-blue"></div>
+<div class="marker"></div>
+<a href="legalities.html" class="left-facing copyrights button red" onclick="beep1()"><span class="margin">Legalities</span></a>
+`;
+
+const beepZero = new Audio('audio/beep-0.mp3');
+const beepOne = new Audio('audio/beep-1.mp3');
+const beepTwo = new Audio('audio/beep-2.mp3');
+
+function beep0() {
+  beepZero.play();
+}
+
+function beep1() {
+  beepOne.play();
+}
+
+function beep2() {
+  beepTwo.play();
+}
+
+function stardateCalculator(selector){
+  document.querySelectorAll(selector).forEach(function(stardate){
+    const date = (selector === '.stardate') ? new Date(stardate.getAttribute('data-date')).getTime() : new Date().getTime();
+    stardate.innerHTML = Math.round(Math.log(date + 432043200000000000000) / Math.log(1.0000001));
+  });
+}
+
+stardateCalculator('.stardate');
+stardateCalculator('.currentStardate');
+setInterval(stardateCalculator, 1000, '.currentStardate');
